eda630d3f208dc720ab09f14c4041fc2846dd8da
JunTanimoto <240080xr@yse-c.net>
2025-06-24 09:52:11 +0900

    jpg

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   images: {
     domains: [
-      'images.unsplash.com' ,  'c.imgz.jp' , 'google.com', 'data:image','www.alotmall.com', 'm.media-amazon.com'
+      'images.unsplash.com' ,  'c.imgz.jp' , 'google.com', 'data:image','www.alotmall.com', 'm.media-amazon.com',
+      'encrypted-tbn3.gstatic.com','item.shachihata.co.jp','www.ikea.com','www.forest.co.jp','jp.images-monotaro.com',
+      'tshop.r10s.jp','www.shop-stationery.com','cdn.askul.co.jp','jp.images-monotaro.com','m.media-amazon.com','cdn.askul.co.jp',
+      'm.media-amazon.com','www.craypas.co.jp','www.hansoku-style.jp','jp.images-monotaro.com','www.lihit-lab.com','tshop.r10s.jp',
+      'www.kokuyo-shop.jp','c.p02.c4a.im','www.yamato.co.jp','image1.shopserve.jp','www.tombow.com',
       // Add more domains here if needed
     ],
   },
