05fd12d7144ba4977ad904ec82ff2ea8b462762c
アムガラン <240046ey@yse-c.net>
2025-06-24 11:48:37 +0900

    last ver

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     domains: [
-      'images.unsplash.com' ,  'c.imgz.jp' , 'google.com', 'data:image','www.alotmall.com', 'm.media-amazon.com', 'www.nichima.co.jp','c.imgz.jp', 'www.nichima.co.jp', 'www.alotmall.com', 'www.google.com', 'www.amazon.co.jp', 'www.amazon.com', 'www.amazon.co.uk', 'www.amazon.de', 'www.amazon.fr', 'www.amazon.it', 'www.amazon.es', 'www.amazon.ca', 'www.amazon.in', 'www.amazon.com.br', 'www.amazon.com.mx', 'www.amazon.com.au', 'www.amazon.co.jp','www.alotmall.com', 'www.nichima.co.jp', 'c.imgz.jp', 'www.google.com', 'www.amazon.co.jp', 'www.amazon.com', 'www.amazon.co.uk', 'www.amazon.de', 'www.amazon.fr', 'www.amazon.it', 'www.amazon.es', 'www.amazon.ca', 'www.amazon.in', 'www.amazon.com.br', 'www.amazon.com.mx', 'www.amazon.com.au','m.media-amazon.com',
+      'images.unsplash.com' ,'www.alotmall.com', 'www.nichima.co.jp', 'c.imgz.jp', 'www.google.com', 'www.amazon.co.jp', 'm.media-amazon.com',
       'encrypted-tbn3.gstatic.com','item.shachihata.co.jp','www.ikea.com','www.forest.co.jp','jp.images-monotaro.com',
       'tshop.r10s.jp','www.shop-stationery.com','cdn.askul.co.jp','jp.images-monotaro.com','m.media-amazon.com','cdn.askul.co.jp',
       'm.media-amazon.com','www.craypas.co.jp','www.hansoku-style.jp','jp.images-monotaro.com','www.lihit-lab.com','tshop.r10s.jp',
